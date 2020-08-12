61d9e5a0bea782c850e8860acb799dcbf973e836
Deepika988 <69334704+Deepika988@users.noreply.github.com>
2020-08-12 11:58:09 +0530

    Add files via upload

---
+++ Circle_Area.java
@@ -0,0 +1,18 @@
+package pack1;
+import java.util.*;
+public class Circle_Area {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String args[]) {
+		// TODO Auto-generated method stub
+		Scanner s= new Scanner(System.in);
+		System.out.println("Enter radius of the circle");
+		float r= s.nextFloat();
+		float area=3.14f*r*r;
+		System.out.println("Area of circle is: "+area);
+		
+	}
+
+}
