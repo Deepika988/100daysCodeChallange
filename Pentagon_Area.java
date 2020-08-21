7fa1a83cb86f6d80af7a488713fc041c189391fb
Deepika988 <69334704+Deepika988@users.noreply.github.com>
2020-08-21 16:36:02 +0530

    Add files via upload

--- Pentagon_Area.java
+++ Pentagon_Area.java
@@ -4,6 +4,13 @@ public class Pentagon_Area {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner obj = new Scanner (System.in);
+		  System.out.println("enter side value:");
+		  int s = obj.nextInt();
+		  System.out.println("enter apothem length:");
+		  int a = obj.nextInt();
+		  float area = (float)(5/2)*s*a;
+		  System.out.println("Area of pentagon = "+area);
 
 	}
 
